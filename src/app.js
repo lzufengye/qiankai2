8e96c26ecb9095f622136e63df46e4c9e38da28a
Guoqing <zhaogq10@163.com>
2016-03-16 03:22:23 +0800

    old theme

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Header from './header'
+import Header from './pages/products/components/header/header'
 import Footer from './footer'
 
 export default React.createClass({
   render() {
     return (
       <div>
-        <Header />
+        <Header display='none'/>
         {this.props.children}
         <Footer />
       </div>
