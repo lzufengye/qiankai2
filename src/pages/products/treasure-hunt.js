c7ff2cadce348aae6b849941a6ce1b7859711f90
Guoqing <zhaogq10@163.com>
2016-03-24 22:37:44 +0800

    hot fix

--- src/pages/products/treasure-hunt.js
+++ src/pages/products/treasure-hunt.js
@@ -19,19 +19,20 @@ var TreasureHunt = React.createClass({
   render: function () {
     var item = {
       id: 6,
-      name: "南瓜子",
-      image: "http://7xjdwj.com1.z0.glb.clouddn.com/images/000/000/012/original/0.jpg",
-      description: "开县南瓜子",
-      price: "27"
+      name: "腊肉",
+      image: "http://7xjdwj.com1.z0.glb.clouddn.com/35.jpg",
+      description: "农户手工熏制，天然无添加剂",
+      price: "39"
     };
     var that = this;
 
     return (
       <div className='treasure-hunt-container'>
         <svg>
-          <circle cx='68%' cy='11%' r='30' fillOpacity="0" onMouseEnter={that.showProduct} onMouseOut={that.hideProduct}></circle>
-          <circle cx='70%' cy='10%' r='50' fillOpacity="0" onMouseEnter={that.showProduct} onMouseOut={that.hideProduct}></circle>
-          <circle cx='73%' cy='10%' r='60' fillOpacity="0" onMouseEnter={that.showProduct} onMouseOut={that.hideProduct}></circle>
+          <circle cx='51%' cy='12%' r='20' fillOpacity="0" onMouseEnter={that.showProduct} onMouseOut={that.hideProduct}></circle>
+          <circle cx='53%' cy='10%' r='30' fillOpacity="0" onMouseEnter={that.showProduct} onMouseOut={that.hideProduct}></circle>
+          <circle cx='56%' cy='12%' r='40' fillOpacity="0" onMouseEnter={that.showProduct} onMouseOut={that.hideProduct}></circle>
+          <circle cx='55.5%' cy='18%' r='10' fillOpacity="0" onMouseEnter={that.showProduct} onMouseOut={that.hideProduct}></circle>
         </svg>
         <ul className='sample-product' onMouseEnter={that.showProduct} onMouseOut={that.hideProduct}>
           <Item key={item.id} item={item} id={item.id} />
