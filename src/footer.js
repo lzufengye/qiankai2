39bf637b6c39e6c59713a164ecd0fe46b4ccb668
Guoqing <zhaogq10@163.com>
2016-04-13 21:30:55 +0800

    added beian number

--- src/footer.js
+++ src/footer.js
@@ -3,7 +3,7 @@ import NavLink from './header/nav-link'
 
 var Footer = React.createClass({
   render() {
-    var className='footer' + (this.props.isMobile ? ' mobile-footer' : '');
+    var className = 'footer' + (this.props.isMobile ? ' mobile-footer' : '');
 
     return (
       <div className={className}>
@@ -16,6 +16,12 @@ var Footer = React.createClass({
         </div>
         <div>
           重庆乾开电子商务有限公司版权所有
+        </div>
+        <div className='beian'>
+          <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=50023402000162" >
+            <img src="/assets/images/beian.png"/>
+            <div>渝公网安备 50023402000162号</div>
+          </a>
         </div>
       </div>
     );
