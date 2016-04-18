51e95bbe9f7eeacc999102398b7949fd1a0ab82e
Guoqing <zhaogq10@163.com>
2016-04-19 00:12:59 +0800

    footer modification

--- src/footer.js
+++ src/footer.js
@@ -11,17 +11,20 @@ var Footer = React.createClass({
           <NavLink to="/about" onlyActiveOnIndex>网站介绍</NavLink>
           |
           <NavLink to="/about" onlyActiveOnIndex>公司介绍</NavLink>
-          |
-          <NavLink to="/about" onlyActiveOnIndex>联系方式</NavLink>
-        </div>
-        <div>
-          重庆乾开电子商务有限公司版权所有
+          <NavLink to="/">联系电话: 400 0933 876</NavLink>
         </div>
         <div className='beian'>
           <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=50023402000162" >
             <img src="/assets/images/beian.png"/>
             <div>渝公网安备 50023402000162号</div>
           </a>
+          <div>ICP备案号：渝ICP备15009385号-1</div>
+        </div>
+        <div className='beian'>
+          <div>食品流通许可证：SP5002341610076106</div>
+        </div>
+        <div>
+          ©2016 重庆乾开电子商务有限公司 版权所有
         </div>
       </div>
     );
