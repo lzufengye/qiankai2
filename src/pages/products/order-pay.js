6012687bb17279aae303cb47574576d76d8b7dda
Guoqing <zhaogq10@163.com>
2016-04-25 02:33:29 +0800

    ping++ payment

--- src/pages/products/order-pay.js
+++ src/pages/products/order-pay.js
@@ -20,7 +20,6 @@ var OrderPay = React.createClass({
     request.get(ServerConfig['serverUrl'] + '/api/transactions/new?sn=' + this.props.params.sn + '&token=' + localStorage.token)
       .end((err, res) => {
         var response = JSON.parse(res.text);
-        console.log(response);
         if (response.transaction.error) {
           layer.msg('支付网关异常，请稍后完成支付');
         } else {
@@ -35,13 +34,30 @@ var OrderPay = React.createClass({
     $('.product-category').css('visibility', 'visible');
   },
 
+  payForOrder: function () {
+    if(this.state.charge) {
+      pingpp.createPayment(this.state.charge, function(result, error){
+        if (result == "success") {
+          layer.msg('支付成功');
+          localStorage.qwezstfp = JSON.stringify({});
+          this.setState({charge: {}});
+          $('.pay-order').attr("disabled","true");
+        } else if (result == "fail") {
+          layer.msg('支付失败，请重试');
+        } else if (result == "cancel") {
+          layer.msg('支付已取消');
+        }
+      });
+    }
+  },
+
   render() {
     var containerClass = 'products-container' + (mobileUtils.mobileCheck() ? ' mobile-products-container' : '')
 
     return (
       <div className={containerClass}>
-        <Order order={this.state.order}/>
-        <button className="btn cart-goto-order pay-order" onClick={this.readyCreateOrder} type="button">{'确认支付'}</button>
+      {<Order order={this.state.order}/>}
+      {<button className="btn cart-goto-order pay-order" onClick={this.payForOrder} type="button">{'确认支付'}</button>} 
       </div>
     );
   }
